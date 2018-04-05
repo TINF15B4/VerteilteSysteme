a5c3e5fe291e8f3000eef8315a48f7bb907fe253
Dominik Schneider <schnedo@web.de>
2018-04-05 13:12:46 +0200

    first interface Restinterface

---
+++ frontend/FrontendQuiz/src/application/RestInterface.java
@@ -0,0 +1,21 @@
+package application;
+
+public class RestInterface {
+	
+	public void postAnswer() {
+		
+	}
+	
+	public void postReady() {
+		
+	}
+	
+	public String getQuestion() {
+		return null;
+	}
+	
+	public int getPoints() {
+		return 0;
+	}
+
+}
