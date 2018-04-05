87df82acc650b14451c999239b384e1141fa02c5
JanikRitz <janikritz@gmail.com>
2018-04-05 13:31:47 +0200

    Basic Functions and TODOs

--- frontend/FrontendQuiz/src/application/NameGenerator.java
+++ frontend/FrontendQuiz/src/application/NameGenerator.java
@@ -1,7 +1,26 @@
 package application;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 public class NameGenerator {
     private final String[] names = {/* TODO */};
 
+    public String getName(){
+        // TODO Call Markov Implementation
+        throw new NotImplementedException();
+    }
+
+    public NameGenerator(){
+        // TODO Setup Map for Lookup
+    }
+
+    private String markovName(int minLength, int maxLength, int chainLength){
+        // TODO Implement Algorithm
+        throw new NotImplementedException();
+    }
+
+    private void generateMap(){
+        // TODO Implement map generation from names
+    }
 
 }
