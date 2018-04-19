c4c5560b5dba4330badc5815618f70f59136ecd6
marco.herglotz <herglotz.m@gmail.com>
2018-04-20 00:48:50 +0200

    backend: some refactorings for my clean code OCD

--- backend/src/main/java/de/tinf15b4/quizduell/db/PersistenceBean.java
+++ backend/src/main/java/de/tinf15b4/quizduell/db/PersistenceBean.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
-import java.util.UUID;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManager;
@@ -46,59 +45,19 @@ public class PersistenceBean {
 
 	public <T> List<T> findAll(Class<T> clazz) {
 		EntityManager manager = factory.createEntityManager();
-		manager.getTransaction().begin();
 		TypedQuery<T> query = manager.createQuery("SELECT x from " + clazz.getName() + " x", clazz);
-		List<T> list = query.getResultList();
-		manager.getTransaction().commit();
-		return list;
+		return query.getResultList();
 	}
 
 	public <T> T findById(Class<T> clazz, Object id) {
-		EntityManager em = factory.createEntityManager();
-		return em.find(clazz, id);
-	}
-
-	public <T> T merge(T o) {
-		EntityManager em = factory.createEntityManager();
-		em.getTransaction().begin();
-		try {
-			o = em.merge(o);
-			em.getTransaction().commit();
-		} catch (Throwable t) {
-			em.getTransaction().rollback();
-			throw t;
-		}
-		return o;
-	}
-
-	public <T> T persist(T o) {
-		EntityManager em = factory.createEntityManager();
-		em.getTransaction().begin();
-		try {
-			em.persist(o);
-			em.getTransaction().commit();
-		} catch (Throwable t) {
-			em.getTransaction().rollback();
-			throw t;
-		}
-		return o;
-	}
-
-	public Game getGameWithId(UUID gameId) {
 		EntityManager manager = factory.createEntityManager();
-		manager.getTransaction().begin();
-		TypedQuery<Game> query = manager.createQuery(
-				"SELECT x from Game x where gameId = '" + gameId.toString().replaceAll("-", "") + "'", Game.class);
-		Game game = query.getSingleResult();
-		manager.getTransaction().commit();
-		return game;
+		return manager.find(clazz, id);
 	}
 
 	public Question getRandomQuestion() {
 		Random rand = new Random();
 		EntityManager manager = factory.createEntityManager();
-		manager.getTransaction().begin();
-		TypedQuery<Long> query = manager.createQuery("SELECT count(*) FROM " + "Question", Long.class);
+		TypedQuery<Long> query = manager.createQuery("SELECT count(*) FROM Question", Long.class);
 
 		Long count = query.getSingleResult();
 		int randomNumber = rand.nextInt(Math.toIntExact(count));
@@ -107,30 +66,21 @@ public class PersistenceBean {
 		selectQuery.setFirstResult(randomNumber);
 		selectQuery.setMaxResults(1);
 		List<Question> resultList = selectQuery.getResultList();
-		manager.getTransaction().commit();
 		return resultList.get(0);
 	}
 
 	public PendingGame findAndConsumePendingGame(User user) {
-		PendingGame g = null;
+		PendingGame game = null;
 
 		EntityManager em = factory.createEntityManager();
-		em.getTransaction().begin();
-		try {
-			TypedQuery<PendingGame> query = em.createQuery(
-					"SELECT x from PendingGame x WHERE x.waitingUser.id <> " + user.getId(), PendingGame.class);
-			query.setMaxResults(1);
-			List<PendingGame> gl = query.getResultList();
-			if (gl.size() > 0) {
-				g = gl.get(0);
-				em.remove(g);
-			}
-			em.getTransaction().commit();
-		} catch (Exception e) {
-			em.getTransaction().rollback();
-			throw e;
+		TypedQuery<PendingGame> query = em.createQuery(
+				"SELECT x from PendingGame x WHERE x.waitingUser.id <> " + user.getId(), PendingGame.class);
+		query.setMaxResults(1);
+		List<PendingGame> queryResult = query.getResultList();
+		if (!queryResult.isEmpty()) {
+			game = queryResult.get(0);
+			transaction().delete(game).commit();
 		}
-
-		return g;
+		return game;
 	}
 }
--- backend/src/main/java/de/tinf15b4/quizduell/rest/services/QuizduellService.java
+++ backend/src/main/java/de/tinf15b4/quizduell/rest/services/QuizduellService.java
@@ -15,9 +15,6 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import de.tinf15b4.quizduell.db.Answer;
 import de.tinf15b4.quizduell.db.Game;
 import de.tinf15b4.quizduell.db.PendingGame;
@@ -34,8 +31,6 @@ public class QuizduellService implements IQuizduellService {
 
 	private static final int ANSWER_TIMEOUT_MILLIS = 20000;
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(QuizduellService.class);
-
 	@Inject
 	private PersistenceBean persistenceBean;
 
@@ -45,21 +40,25 @@ public class QuizduellService implements IQuizduellService {
 	@Produces(MediaType.APPLICATION_JSON)
 	public QuestionDTO getQuestion(@PathParam("gameId") UUID gameId, @PathParam("userId") long userId) {
 		Game game = persistenceBean.findById(Game.class, gameId);
-		if (game == null)
-			throw new WebApplicationException(Response.status(404).entity("Unknown Game").build());
-
-		if (game.getCurrentQuestion() == null)
-			throw new WebApplicationException(204);
-
-		if (game.getCurrentUser().getId() != userId)
-			throw new WebApplicationException(423);
+		checkPreconditions(userId, game);
 
 		if (game.getTimestamp() == -1) {
 			game.setTimestamp(System.currentTimeMillis());
-			game = persistenceBean.merge(game);
+			persistenceBean.transaction().update(game).commit();
 		}
 
 		return game.getCurrentQuestion().toDTO();
+	}
+
+	private void checkPreconditions(long userId, Game game) {
+		if (game == null)
+			throw new WebApplicationException(Response.status(404).entity("Unknown Game").build());
+
+		if (game.getCurrentUser().getId() != userId)
+			throw new WebApplicationException(Response.status(423).entity("It is the other user's turn").build());
+
+		if (game.getCurrentQuestion() == null)
+			throw new WebApplicationException(Response.status(204).entity("Game is finished").build());
 	}
 
 	@Override
@@ -68,17 +67,17 @@ public class QuizduellService implements IQuizduellService {
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void postAnswer(Answer answer, @PathParam("gameId") UUID gameId, @PathParam("userId") long userId) {
 		Game game = persistenceBean.findById(Game.class, gameId);
-		if (game == null)
-			throw new WebApplicationException(Response.status(404).entity("Unknown Game").build());
+		checkPreconditions(userId, game);
 
-		if (game.getCurrentUser().getId() != userId)
-			throw new WebApplicationException(423);
+		updateUsersAndTimestamp(game);
+		if (checkAnswer(answer, game)) {
+			game.getCurrentPlayingUser().incrementPoints();
+			persistenceBean.transaction().update(game.getCurrentPlayingUser()).commit();
+		}
 
-		Question answeredQuestion = game.getCurrentQuestion();
-		long answerTimestamp = game.getTimestamp();
-		if (answeredQuestion == null)
-			throw new WebApplicationException(Response.status(400).entity("Game finished").build());
-
+	}
+	
+	private void updateUsersAndTimestamp(Game game) {
 		if (game.getCurrentUser().getId() == game.getUser1().getUser().getId()) {
 			// user 1 --> switch to user 2
 			game.setCurrentUser(game.getUser2().getUser());
@@ -88,26 +87,23 @@ public class QuizduellService implements IQuizduellService {
 			game.nextQuestion();
 		}
 		game.setTimestamp(-1);
-		game = persistenceBean.merge(game);
+		persistenceBean.transaction().update(game).commit();
+	}
 
-		// check validity and award points
-		if (answeredQuestion.getCorrectAnswer().equals(answer) || answeredQuestion.getAnswers().contains(answer)) {
-			long diff = System.currentTimeMillis() - answerTimestamp;
-			if (diff > ANSWER_TIMEOUT_MILLIS) {
-				throw new WebApplicationException(
-						Response.status(406).entity("Answer too late. Timeout has been reached").build());
-			}
-			if (answeredQuestion.getCorrectAnswer().equals(answer)) {
-				game.getCurrentPlayingUser().incrementPoints();
-				persistenceBean.transaction().update(game.getCurrentPlayingUser()).commit();
-			} else {
-				throw new WebApplicationException(Response.status(406).entity("Answer was wrong").build());
-			}
-		} else {
-			// recieved an Answer that does not match the answers of the question
+	private boolean checkAnswer(Answer answer, Game game) {
+		if (System.currentTimeMillis() - game.getTimestamp() > ANSWER_TIMEOUT_MILLIS) {
+			throw new WebApplicationException(
+					Response.status(406).entity("Answer too late. Timeout has been reached").build());
+		}
+
+		Question answeredQuestion = game.getCurrentQuestion();
+		if (answeredQuestion.getCorrectAnswer().equals(answer))
+			return true;
+		if (!answeredQuestion.getAnswers().contains(answer)) {
 			throw new WebApplicationException(
 					Response.status(400).entity("Answer not in question's answer set").build());
 		}
+		throw new WebApplicationException(Response.status(406).entity("Answer was wrong").build());
 	}
 
 	@Override
@@ -116,31 +112,31 @@ public class QuizduellService implements IQuizduellService {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/ready")
 	public UUID ready(long userid) {
-		User u = persistenceBean.findById(User.class, userid);
-		if (u == null)
-			throw new WebApplicationException(Response.status(400).entity("Unknown user\n").build());
+		User user = persistenceBean.findById(User.class, userid);
+		if (user == null)
+			throw new WebApplicationException(Response.status(400).entity("Unknown user").build());
 
-		PendingGame pg = persistenceBean.findAndConsumePendingGame(u);
-		if (pg != null) {
+		PendingGame pendingGame = persistenceBean.findAndConsumePendingGame(user);
+		if (pendingGame != null) {
 			// create game
 			List<Question> questionList = new ArrayList<>();
 			for (int i = 0; i < 5; i++) {
 				questionList.add(persistenceBean.getRandomQuestion());
 			}
-			Game g = new Game(pg.getGameId(), new PlayingUser(u, 0), new PlayingUser(pg.getWaitingUser(), 0),
+			Game game = new Game(pendingGame.getGameId(), new PlayingUser(user, 0), new PlayingUser(pendingGame.getWaitingUser(), 0),
 					questionList);
-			g.setCurrentUser(u);
-			g.setTimestamp(-1);
+			game.setCurrentUser(user);
+			game.setTimestamp(-1);
 
-			persistenceBean.merge(g);
-			return g.getGameId();
+			persistenceBean.transaction().update(game).commit();
+			return game.getGameId();
 		} else {
 			// create new pending game
 
-			pg = new PendingGame(u);
-			persistenceBean.merge(pg);
+			pendingGame = new PendingGame(user);
+			persistenceBean.transaction().update(pendingGame).commit();
 
-			return pg.getGameId();
+			return pendingGame.getGameId();
 		}
 	}
 
@@ -149,7 +145,7 @@ public class QuizduellService implements IQuizduellService {
 	@Path("/points/{gameId}/{userId}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Points getPoints(@PathParam("gameId") UUID gameId, @PathParam("userId") long userId) {
-		Game game = persistenceBean.getGameWithId(gameId);
+		Game game = persistenceBean.findById(Game.class, gameId);
 		int myPoints = -1;
 		int otherPoints = 0;
 
@@ -158,11 +154,11 @@ public class QuizduellService implements IQuizduellService {
 			otherPoints = game.getUser2().getPoints();
 		} else if (game.getUser2().getId() == userId) {
 			myPoints = game.getUser2().getPoints();
-			otherPoints = game.getUser2().getPoints();
+			otherPoints = game.getUser1().getPoints();
 		}
 
 		if (myPoints == -1)
-			throw new WebApplicationException(Response.status(400).entity("Invalid user id").build());
+			throw new WebApplicationException(Response.status(400).entity("Unknown user").build());
 		return new Points(myPoints, otherPoints);
 	}
 
@@ -172,10 +168,10 @@ public class QuizduellService implements IQuizduellService {
 	@Consumes("application/json")
 	@Produces("application/json")
 	public long createUser(String username) {
-		User u = new User();
-		u.setUsername(username);
-		persistenceBean.persist(u);
-		return u.getId();
+		User user = new User();
+		user.setUsername(username);
+		persistenceBean.transaction().persist(user).commit();
+		return user.getId();
 	}
 
 }
