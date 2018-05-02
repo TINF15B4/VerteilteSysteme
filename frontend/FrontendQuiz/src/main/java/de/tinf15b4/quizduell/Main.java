567514f5592c7651ee31e1f73e0f7b2f6cf41495
Dominik Schneider <schnedo@web.de>
2018-05-02 12:05:26 +0200

    restructured project to conform mvn standards & made jar executable

--- frontend/FrontendQuiz/src/main/java/de/tinf15b4/quizduell/Main.java
+++ frontend/FrontendQuiz/src/main/java/de/tinf15b4/quizduell/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("Test.fxml"));
+		Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Test.fxml"));
 
 		Scene scene = new Scene(root, 600, 600);
 
